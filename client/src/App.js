e3a46131a69f089b8a345033836ecd9d505a55c5
Yeasar <yeasar1507099@stud.kuet.ac.bd>
2024-01-21 20:15:49 +0600

    routing solved

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,32 @@
-// import React, { useEffect, useState } from "react";
+// App.js
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Home";
 import EmployeeList from "./EmployeeList";
 
-function App() {
-  // const [backendData, setBackendData] = useState([{}]);
-
-  // useEffect(() => {
-  //   fetch("api/")
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       setBackendData(data);
-  //     });
-  // }, []);
-  // console.log(backendData);
+const App = () => {
   return (
-    <div>
-      <EmployeeList />
-    </div>
+    <Router>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/employees" element={<EmployeeList />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
+
+// import React, { useEffect, useState } from "react";
+// import EmployeeList from "./EmployeeList";
+
+// function App() {
+//   return (
+//     <div>
+//       <EmployeeList />
+//     </div>
+//   );
+// }
+
+// export default App;
